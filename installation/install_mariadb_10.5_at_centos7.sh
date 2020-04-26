47efef63f9c99f15e414820bdf3719b0b752ae1b
wnlin <heavenruler@users.noreply.github.com>
2020-04-26 13:56:45 +0800

    add 10.5 version

---
+++ installation/install_mariadb_10.5_at_centos7.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# you need clean or minimal install OS env.
+
+# get rhel7 first.
+yum clean all ; yum update ; yum upgrade -y
+yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+
+# get mariadb repo config 
+# Source Ref: https://downloads.mariadb.com/MariaDB/
+curl -sS https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | bash
+
+# The last supported version of centos7 is mariadb 5.5
+sed -i 's/10.4/10.5/' /etc/yum.repos.d/mariadb.repo
+
+# Installing
+yum clean all ; yum -y install MariaDB-server MariaDB-shared MariaDB-common
+
+# Checking systemd identify name
+systemctl status mariadb
+
+# Disable mariadb running after rebooted
+systemctl disable mariadb
+
+# MariaDB Started.
+systemctl start mariadb
+
+# Check mariadb status.
+systemctl status mariadb
