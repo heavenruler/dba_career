ceb130e63b191d75bc264e18ca3fd2298ece3de9
wn.lin <wn.lin@104.com.tw>
2026-04-15 14:16:21 +0800

    update

--- poc/iac/main.tf
+++ poc/iac/main.tf
@@ -5,9 +5,71 @@ provider "vsphere" {
   allow_unverified_ssl = true
 }
 
-# Connection test: list datacenters
-data "vsphere_datacenter" "all" {}
+data "vsphere_datacenter" "dc" {
+  name = "sd-datacenter"
+}
 
-output "datacenter_id" {
-  value = data.vsphere_datacenter.all.id
+data "vsphere_compute_cluster" "cluster" {
+  name          = "sd-cluster"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_datastore" "vm_ds" {
+  name          = "D1_D_DBA_dev_poc"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_datastore" "iso_ds" {
+  name          = "D1_D_DBA_dev_worker"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_network" "network" {
+  name          = "DBA/VLAN241-172.24.32.0/20"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_resource_pool" "pool" {
+  name          = "DB-DEV"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+resource "vsphere_virtual_machine" "poc" {
+  name             = var.vm_name
+  resource_pool_id = data.vsphere_resource_pool.pool.id
+  datastore_id     = data.vsphere_datastore.vm_ds.id
+
+  num_cpus             = var.vm_cpu
+  memory               = var.vm_memory
+  guest_id             = "almaLinux64Guest"
+  firmware             = "efi"
+  hardware_version     = 19
+
+  network_interface {
+    network_id   = data.vsphere_network.network.id
+    adapter_type = "vmxnet3"
+  }
+
+  disk {
+    label            = "disk0"
+    size             = var.vm_disk_size
+    thin_provisioned = true
+  }
+
+  cdrom {
+    datastore_id = data.vsphere_datastore.iso_ds.id
+    path         = "0_ISO/AlmaLinux-10.1-x86_64-minimal.iso"
+  }
+
+  # 不等 VMware Tools（ISO 裝機時尚未安裝）
+  wait_for_guest_net_timeout = 0
+  wait_for_guest_ip_timeout  = 0
+}
+
+output "vm_name" {
+  value = vsphere_virtual_machine.poc.name
+}
+
+output "vm_uuid" {
+  value = vsphere_virtual_machine.poc.id
 }
--- poc/iac/variables.tf
+++ poc/iac/variables.tf
@@ -12,3 +12,23 @@ variable "vsphere_server" {
   type    = string
   default = "sd-vcsa.e104.com.tw"
 }
+
+variable "vm_name" {
+  type    = string
+  default = "l-test-poc-1"
+}
+
+variable "vm_cpu" {
+  type    = number
+  default = 4
+}
+
+variable "vm_memory" {
+  type    = number
+  default = 16384  # 16 GB in MB
+}
+
+variable "vm_disk_size" {
+  type    = number
+  default = 100  # GB
+}
